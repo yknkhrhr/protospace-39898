400f8d74e3b799846f5529912ad94d315a653d52
yknkhrhr <146395389+yknkhrhr@users.noreply.github.com>
2023-11-29 10:52:56 +0900

    デプロイ前の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,9 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
-      render "prototype/show"
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show"
     end
   end
 
